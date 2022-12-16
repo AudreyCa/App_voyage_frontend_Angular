f55d478c75daa77c10e0125600f84148db755910
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2022-12-16 11:11:03 +0100

    ajout de electron et ionic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // tiles: any = [
-  //   {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-  //   {text: 'Two', cols: 1, rows: 9, color: 'lightgreen'},
-  //   {text: 'Three', cols: 3, rows: 8, color: 'lightpink'}
-  // ]
 }
