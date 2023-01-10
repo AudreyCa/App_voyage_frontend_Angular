fc221fa12afcc3ba0b0ed00a2ff95a552efe9434
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2023-01-10 16:02:42 +0100

    chagt pour electron

--- main.js
+++ main.js
@@ -21,42 +21,39 @@ function createWindow() {
     })
  
     // adresse de l'appli :
-    win.loadURL('http://localhost:4200')
-    // si j'avais lié avec un fichier et non une url :
-    // win.loadURL(path.join(__dirname,'client/dist/client/index.html'))  
- 
+    win.loadURL('https://organisez-vos-voyages.onrender.com/')
+    // win.loadURL('http://localhost:4200')
+    
+    // pour les outils:
     win.webContents.openDevTools()
  
 }
 
 
 
-// Quitter quand toutes les fenêtres sont fermées, SAUF SUR macOS. Dans ce cas il est courant que les applications et barre de menu restent actives jusqu'à ce que l'utilisateur quitte de manière explicite par Cmd + Q.
-app.on('window-all-closed', () => {
- 
-    if (process.platform !== 'darwin') {
- 
-        app.quit()
-    }
-})
-
-
-
-
-// Appelons cette fonction createWindow() pour ouvrir notre fenêtre.
+// Appelons la fonction createWindow(), ci dessus, pour ouvrir notre fenêtre.
 // Cette méthode sera appelée quand Electron aura fini de s'initialiser et sera prêt à créer des fenêtres de navigation.
 // Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
-  app.whenReady().then(() => {
+  app.on('ready', () => {
     createWindow()
 
     app.on('activate', () => {
       // Sur macOS il est commun de re-créer une fenêtre  lors 
       // du click sur l'icone du dock et qu'il n'y a pas d'autre fenêtre ouverte.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
     })
 
   })
-
-
-
-
+  
+  
+   
+  
+  // Quitter quand toutes les fenêtres sont fermées, SAUF SUR macOS. Dans ce cas il est courant que les applications et barre de menu restent actives jusqu'à ce que l'utilisateur quitte de manière explicite par Cmd + Q.
+  app.on('window-all-closed', () => {
+   
+      if (process.platform !== 'darwin') {
+          app.quit()
+      }
+  })
