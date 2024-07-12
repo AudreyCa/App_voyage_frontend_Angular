b9cdd6bf2363d9e1bc40ddbb0fbd96921246171b
Aud Can <a.cannesson5@gmail.com>
2024-07-12 13:43:26 +0200

    modif pour deploiement

--- main.js
+++ main.js
@@ -6,27 +6,26 @@ const { app, BrowserWindow } = require('electron')
 
 // puis, ajoutons une fonction createWindow() qui charge index.html dans une nouvelle instance BrowserWindow.
 function createWindow() {
- 
     // Création de la fenêtre de navigateur.
     const win = new BrowserWindow({
- 
+
         width: 800,
- 
+
         height: 600,
- 
+
         webPreferences: {
- 
+
             nodeIntegration: true
         }
     })
- 
+
     // adresse de l'appli :
     win.loadURL('https://organisez-vos-voyages.onrender.com/')
     // win.loadURL('http://localhost:4200')
     
     // pour les outils:
     win.webContents.openDevTools()
- 
+
 }
 
 
@@ -48,11 +47,8 @@ function createWindow() {
   })
   
   
-   
-  
   // Quitter quand toutes les fenêtres sont fermées, SAUF SUR macOS. Dans ce cas il est courant que les applications et barre de menu restent actives jusqu'à ce que l'utilisateur quitte de manière explicite par Cmd + Q.
   app.on('window-all-closed', () => {
-   
       if (process.platform !== 'darwin') {
           app.quit()
       }
